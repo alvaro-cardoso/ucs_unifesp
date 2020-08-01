57a7790f44483580dde06861897d1ea9dfa69e45
Álvaro Cardoso Vicente de Souza <alvarocv.souza@gmail.com>
2020-08-01 00:45:14 -0300

    add ex 7

---
+++ Lógica_Prog/ex 7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+    int n;
+    Printf(“digite o numero”);
+    Scanf(“% d”, &amp; n);
+    If(n == 5)
+        Printf(“o numero eh igual a 5 ”);
+    Else if (n == 400)
+        Printf(“o numero eh igual a 400”);
+    Else if (n &gt; = 500) & amp;
+    &amp;
+    (n &lt; = 1000)
+        Printf(“o numero esta entre 500 e 1000”);
+    Else
+    Printf(“o numero nao se enquadra nos escopos ”);
+}
